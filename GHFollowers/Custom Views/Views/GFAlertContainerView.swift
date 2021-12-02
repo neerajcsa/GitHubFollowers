13184c44724dfcfb157fa0fb8c37d14b8d4116cd
Neeraj Pandey <neerajcsa@yahoocom>
2021-12-02 22:19:38 +0530

    GH Followers

---
+++ GHFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by Sean Allen on 1/31/20.
+//  Copyright © 2020 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        backgroundColor       = .systemBackground
+        layer.cornerRadius    = 16
+        layer.borderWidth     = 2
+        layer.borderColor     = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
